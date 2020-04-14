cfa924aa54cef75e12c409c6d88bbde647bfea81
Jochen Winzer <jw@winzer-sw.de>
2020-04-14 15:25:18 +0200

    Changed __url__. Changed Standard View to Default View. Updated translations.

--- openslides/__init__.py
+++ openslides/__init__.py
@@ -2,6 +2,6 @@ __author__ = 'OpenSlides Team <support@openslides.org>'
 __description__ = 'Presentation and assembly system'
 __version__ = '2.3.2-dev'
 __license__ = 'MIT'
-__url__ = 'https://openslides.org'
+__url__ = 'https://github.com/jwinzer/OpenSlides/tree/votehome'
 
 args = None
